8eb73244edb8e47fbed4b45ab3cfbccc8eef4889
davipatricio <davipatricio@pm.me>
2021-08-21 22:36:46 -0300

    oopsie.

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const fetch = require('node-fetch');
 
 async function get (url, parseAs = 'json', method) {
 	const request = await fetch(`${url}`, { method: method ?? 'GET' });
-	return request[parseAs]?.() ?? request[parseAs].json();
+	return request[parseAs]?.() ?? request.json();
 }
 
 module.exports = {
