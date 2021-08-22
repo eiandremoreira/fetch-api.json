8816a3a15c1da1b36659071f6cfd35ea8db42f42
davipatricio <davipatricio@pm.me>
2021-08-22 11:27:09 -0300

    smol fix

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const fetch = require('node-fetch');
 
 async function get (url, parseAs = 'json', method = 'GET') {
-	if (!typeof url !== 'string') return throw new Error('A URL precisa ser definida e deve ser do tipo STRING.');
+	if (typeof url !== 'string') return throw new Error('A URL precisa ser definida e deve ser do tipo STRING.');
 
 	const request = await fetch(url, { method: method });
 	return request[parseAs]?.() ?? request.json();
