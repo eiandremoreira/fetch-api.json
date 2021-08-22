9b5772bebe6d6b28a5092f48259304f2135972db
davipatricio <davipatricio@pm.me>
2021-08-21 22:53:06 -0300

    não é necessário

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const fetch = require('node-fetch');
 
 async function get (url, parseAs = 'json', method = 'GET') {
-	const request = await fetch(`${url}`, { method: method });
+	const request = await fetch(url, { method: method });
 	return request[parseAs]?.() ?? request.json();
 }
 
