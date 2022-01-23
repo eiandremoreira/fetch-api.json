1ce4ae3ceb757485fe8848a51a158b162b6da768
Davi Patricio <davipatricio@pm.me>
2022-01-23 12:46:23 -0300

    remove unnecessary statements & support to ESM

--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
 const fetch = require('node-fetch');
 
 async function get (url, parseAs = 'json', method = 'GET') {
-	if (typeof url !== 'string') return throw new Error('A URL precisa ser definida e deve ser do tipo STRING.');
+	if (typeof url !== 'string') throw new Error('A URL precisa ser definida e deve ser do tipo STRING.');
 
-	const request = await fetch(url, { method: method });
+	const request = await fetch(url, { method });
 	return request[parseAs]?.() ?? request.json();
 }
 
-module.exports = {
-	get
-};
+exports = { get };
